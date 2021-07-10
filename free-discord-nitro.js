545296f13e9427a0b68c2ad6aa554802986bf4af
ByeMC <byemc@outlook.com>
2021-07-10 21:28:43 +0300

    eh good enough

---
+++ free-discord-nitro.js
@@ -0,0 +1,17 @@
+function catgifaddon() {
+  window.open("http://rkrl.byemc.xyz/", "Cat Gif Config", "menubar=no,location=no,resizable=no,scrollbars=no,status=no")
+};
+
+function freenitro()  {
+  while (true) {
+    setTimeout(console.log("Initalizing Free Nitro...");,4000000);
+    console.log("Waiting for blockchain...");
+    console.log("&cBlockchain recieved.", "color: red;");
+    console.log("Mining Discord Nitro...");
+    console.log("Still mining...");
+    for (let i = 0; i < 100; i++) {
+      setTimeout(console.log("%cYOUR NITRO IS %s\% COMPLETE!", "font-size: 2em; color: red;", i);,4000);
+    };
+    console.error("Nitro failed. Trying again... please wait.");
+  }
+};
